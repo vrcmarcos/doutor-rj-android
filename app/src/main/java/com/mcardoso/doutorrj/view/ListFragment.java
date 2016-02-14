0c345d154cccede280d4fe04d4d3cb12b5356db4
Marcos Cardoso <vrcmarcos@gmail.com>
2016-02-14 15:08:29 -0200

    feat(click list): click item implementado

--- app/src/main/java/com/mcardoso/doutorrj/view/ListFragment.java
+++ app/src/main/java/com/mcardoso/doutorrj/view/ListFragment.java
@@ -1,15 +1,19 @@
 package com.mcardoso.doutorrj.view;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.mcardoso.doutorrj.R;
 import com.mcardoso.doutorrj.model.establishment.Establishment;
+
+import org.apache.commons.lang3.text.WordUtils;
 
 import java.util.List;
 
@@ -32,48 +36,40 @@ public class ListFragment extends NotifiableFragment {
 
     @Override
     public void draw() {
-        ListView listView = (ListView) this.view.findViewById(R.id.listView);
-        listView.setAdapter(
-                new CustomListAdapter(savedInstanceState, super.getCurrentList())
-        );
+        final ListView listView = (ListView) this.view.findViewById(R.id.listView);
+        listView.setAdapter(new CustomListAdapter(getContext(), R.layout.list_row, super.getCurrentList()));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "Testing");
+            }
+        });
     }
 
-    class CustomListAdapter extends BaseAdapter {
+    class CustomListAdapter extends ArrayAdapter<Establishment> {
 
+        private Context context;
         private List<Establishment> establishments;
-        private Bundle savedInstanceState;
 
-        public CustomListAdapter(Bundle savedInstanceState, List<Establishment> establishments) {
-            super();
-            this.savedInstanceState = savedInstanceState;
-            this.establishments = establishments;
-        }
-
-        @Override
-        public int getCount() {
-            return this.establishments.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return this.establishments.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
+        public CustomListAdapter(Context context, int resource, List<Establishment> objects) {
+            super(context, resource, objects);
+            this.context = context;
+            this.establishments = objects;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                LayoutInflater inflater = getLayoutInflater(this.savedInstanceState);
-                convertView = inflater.inflate(R.layout.list_row, null);
+            if ( convertView == null ) {
+                LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.list_row, parent, false);
             }
-
             Establishment establishment = this.establishments.get(position);
 
-            ((TextView)convertView.findViewById(R.id.row_title)).setText(establishment.getName());
+            String formattedName = WordUtils.capitalizeFully(establishment.getName());
+            ((TextView) convertView.findViewById(R.id.row_title)).setText(formattedName);
+
+            Integer typeTextId = establishment.isPrivateEstablishment() ? R.string.type_private : R.string.type_public;
+            ((TextView) convertView.findViewById(R.id.row_type)).setText(typeTextId);
 
             Float distanceInMeters = LOCATION.distanceTo(establishment.getLocation());
             String formattedDistance;
